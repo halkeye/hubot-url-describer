fead53ea1316642bf713fb84b8f55d0ff7e11f65
Gavin Mogan <git@gavinmogan.com>
2020-11-11 19:34:46 -0800

    Fix tests and disable coffeescript

--- src/scripts/hubot-url-describer.js
+++ src/scripts/hubot-url-describer.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 // Description:
 //   The best project ever.
 //
@@ -27,7 +22,7 @@ const request = require('request');
 const Url = require('url');
 const Querystring = require('querystring');
 const Path = require('path');
-const HtmlParser = require('htmlparser2');
+const cheerio = require('cheerio');
 const unescapeHTML = require('unescape-html');
 
 let ignore_extensions = ['.png','.jpg','.jpeg','.gif','.txt','.zip','.tar.bz','.js','.css'];
@@ -38,36 +33,6 @@ const regex = /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\
 module.exports = function(robot) {
   robot.hear(regex, function(msg) {
     const url = msg.match[0];
-
-    let title = "";
-    const tagstack = [];
-
-    const parser  = new HtmlParser.Parser({
-      onopentag(name, attribs) {
-        tagstack.push(name);
-        if ( (title !== "") && (name === 'twitter:title') ) {
-          return title = attribs['content'].trim().split("\n")[0];
-        }
-      },
-      ontext(text) {
-        const tagname = tagstack[tagstack.length - 1];
-        if (tagname === "title") {
-          return title = text.trim().split("\n")[0];
-        }
-      },
-        //else if ( title != "" and tagname == 'twitter:title' )
-      onclosetag(tagname) {
-        return tagstack.pop();
-      },
-      onend() {
-        if (title.length > 0) {
-          return msg.send(unescapeHTML(title.replace('&mdash;', '--')));
-        } else {
-          return msg.send('No title found');
-        }
-      }
-    });
-
     const urldata = Url.parse(url);
     const path = urldata.path.split('?')[0];
     const ext = Path.extname(path);
@@ -83,11 +48,17 @@ module.exports = function(robot) {
       if (res.headers['content-type'].indexOf('text/html') !== 0) {
         return;
       }
-      parser.write(body);
-      return parser.end();
+      const $ = cheerio.load(body);
+      let title = $('title').text() || $('[name="twitter:title"]').attr('content') || $('[name="og:title"]').attr('content') || '';
+      title = title.split('\n').map(t => t.trim()).filter(Boolean)[0];
+      if (title && title.length > 0) {
+        return msg.send(unescapeHTML(
+          title.replace(/&mdash;|\u2014/g, '--')
+        ));
+      } else {
+        return msg.send('No title found');
+      }
     });
-    return this;
   });
-  return this;
 };
 
